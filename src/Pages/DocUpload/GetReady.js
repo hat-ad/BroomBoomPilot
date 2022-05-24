a47ff25bac327721ce36d3ce841dc470ff974185
Souvik Bhattacharyya <57744600+Souvik-Bhattacharyya@users.noreply.github.com>
2022-05-24 17:41:17 +0530

    some extra pages

--- src/Pages/DocUpload/GetReady.js
+++ src/Pages/DocUpload/GetReady.js
@@ -1,26 +1,40 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 import { CircleCheckIcon } from "../../Utility/iconLibrary";
 
+const styles = {
+  yel: {
+    color: "#333",
+    fontSize: 22,
+    fontWeight: "700",
+    marginLeft: 26,
+    lineHeight: 25,
+  }
+}
+
 const GetReady = () => {
   return (
-    <View>
-      <View>
-        <Text>Make sure you keep following document ready</Text>
+    <View style={{ padding: 10,alignItems: "center",flex:1,justifyContent: "space-between"}}>
+      <View style={{ width: "100%",alignItems: "center" }}>
+        <Text style={{ fontSize: 22, textAlign: "center", fontWeight: "400", width: "65%", marginBottom: 92, marginTop: 64 }}>Make sure you keep following document ready</Text>
         <View>
-          <CircleCheckIcon />
-          <Text>Driving License</Text>
-        </View>
-        <View>
-          <CircleCheckIcon />
-          <Text>Vehhicle Rc</Text>
-        </View>
-        <View>
-          <CircleCheckIcon />
-          <Text>Aadhaar/PAN card</Text>
+          <View style={{flexDirection:"row", marginBottom:50}}>
+            <CircleCheckIcon color={"#F5C001"} size={22}/>
+            <Text style={styles.yel}>Driving License</Text>
+          </View>
+          <View style={{flexDirection:"row", marginBottom:50}}>
+            <CircleCheckIcon color={"#F5C001"} size={22} />
+            <Text style={styles.yel}>Vehhicle Rc</Text>
+          </View>
+          <View style={{flexDirection:"row", marginBottom:50}}>
+            <CircleCheckIcon  color={"#F5C001"} size={22}/>
+            <Text style={styles.yel}>Aadhaar/PAN card</Text>
+          </View>
         </View>
       </View>
-      <View></View>
+      <View style={{marginBottom: 50}}>
+        <Image source={require("../../../assets/BigLoader.png")} />
+      </View>
     </View>
   );
 };
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -2,4 +2,6 @@ import Welcome from "./Welcome";
 import DrivingLicense from "./DocUpload/DrivingLicense";
 import Language from "./Language/index";
 import SignUp from "./SignUp/index";
-export { Welcome, Language, SignUp, DrivingLicense };
+import GetReady from "./DocUpload/GetReady";
+import vehicleRC from "./DocUpload/vehicleRC";
+export { Welcome, Language, SignUp, DrivingLicense, GetReady, vehicleRC };
