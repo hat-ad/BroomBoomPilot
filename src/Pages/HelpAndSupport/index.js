6f69b0ddeca81fe9cf6387be57ecbf0ce0eeb659
Souvik Bhattacharyya <57744600+Souvik-Bhattacharyya@users.noreply.github.com>
2022-05-30 02:28:44 +0530

    help and support add

--- src/Pages/HelpAndSupport/index.js
+++ src/Pages/HelpAndSupport/index.js
@@ -2,15 +2,57 @@ import { View, Text, TouchableOpacity, Image } from "react-native";
 import React, { useState } from "react";
 import styles from "./style";
 import metrics from "../../Utility/metrics";
-import { SearchIcon } from "../../Utility/iconLibrary";
+import { SearchIcon, ArrowRight } from "../../Utility/iconLibrary";
 
 const HelpAndSupport = () => {
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={{ flexDirection: "row" }}>
-        <SearchIcon />
-        <Text style={{ fontSize: 24 }}>Search Issue</Text>
+      <TouchableOpacity style={styles.search}>
+        <SearchIcon size={24} style={{ paddingHorizontal: metrics.scale(20) }} />
+        <Text style={{ fontSize: 16, color: "#828282", fontWeight: "600" }}>Search Issue</Text>
       </TouchableOpacity>
+
+      <View style={styles.cardCont}>
+        <View style={styles.cards}>
+          <View>
+            <Text style={{ fontSize: 16, color: "#333", fontWeight: "700" }}>FAQs</Text>
+            <Text style={{ fontSize: 12, color: "#aaa", fontWeight: "600" }}>Some long long text here </Text>
+            <TouchableOpacity style={{ marginTop: metrics.verticalScale(10) }}>
+              <Text style={{ fontSize: 10, color: "#347eea" }}>
+                Click Here <ArrowRight size={10} style={{ color: "#347eea" }} />
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <Image source={require("../../../assets/Icon/faqIcon.png")} style={{ width: 44, height: 44, marginLeft: "auto" }} />
+        </View>
+
+        <View style={styles.cards}>
+          <View>
+            <Text style={{ fontSize: 16, color: "#333", fontWeight: "700" }}>Payment and Wallets</Text>
+            <Text style={{ fontSize: 12, color: "#aaa", fontWeight: "600" }}>Your Payments and Wallet balance</Text>
+            <TouchableOpacity style={{ marginTop: metrics.verticalScale(10) }}>
+              <Text style={{ fontSize: 10, color: "#347eea" }}>
+                Click Here <ArrowRight size={10} style={{ color: "#347eea" }} />
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <Image source={require("../../../assets/Icon/walletIcon.png")} style={{ width: 44, height: 44, marginLeft: "auto" }} />
+        </View>
+
+        <View style={styles.cards}>
+          <View>
+            <Text style={{ fontSize: 16, color: "#333", fontWeight: "700" }}>Contact 24x7 Help</Text>
+            <Text style={{ fontSize: 12, color: "#aaa", fontWeight: "600" }}>Our excutive will help solve your problem</Text>
+            <TouchableOpacity style={{ marginTop: metrics.verticalScale(10) }}>
+              <Text style={{ fontSize: 10, color: "#347eea" }}>
+                Click Here <ArrowRight size={10} style={{ color: "#347eea" }} />
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <Image source={require("../../../assets/Icon/contactIcon.png")} style={{ width: 44, height: 44, marginLeft: "auto" }} />
+        </View>
+      </View>
+
       <Text style={styles.anchorTag}>
         For more information read our{" "}
         <Text style={{ color: "#347EEA" }}> terms and condition </Text>
--- src/Pages/HelpAndSupport/style.js
+++ src/Pages/HelpAndSupport/style.js
@@ -8,15 +8,39 @@ export default styles = StyleSheet.create({
         alignItems: "center",
         flex: 1,
         justifyContent: "space-between",
-    },
-    loader: {
-        position: "absolute",
-        bottom: 40
+        backgroundColor: "#F9F9F9"
     },
     anchorTag: {
         fontSize: 14,
         textAlign: "center",
         position: "absolute",
         bottom: 20
+    },
+    search: {
+        flexDirection: "row",
+        width: "100%",
+        padding: metrics.verticalScale(16),
+        borderColor: "#000",
+        borderRadius: 50,
+        borderWidth: 1,
+        backgroundColor: "#F8EEC7",
+        alignItems: "center"
+    },
+    cards: {
+        height: 100,
+        width: "100%",
+        flexDirection: "row",
+        backgroundColor: "#fff",
+        paddingVertical: metrics.verticalScale(12),
+        paddingHorizontal: metrics.scale(20),
+        borderRadius: 10,
+        borderColor: "#E6E6E6",
+        borderWidth: 1,
+        marginBottom: metrics.verticalScale(10),
+    },
+    cardCont: {
+        height: "50%",
+        position: "absolute",
+        top: "15%"
     }
 });
